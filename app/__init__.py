1106cfae43dc5001196977deb29f9860d62141c7
ademirsoares <ademir@licitaronline.com>
2026-06-30 18:40:19 -0300

    chore: configura infraestrutura para integração com PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from app.config import Config
+from app.database import db
 from app.routes import init_app
 
 
@@ -9,6 +10,8 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_object(Config)
+
+    db.init_app(app)
 
     init_app(app)
 
--- app/config.py
+++ app/config.py
@@ -3,3 +3,7 @@ class Config:
     SECRET_KEY = "gestao-ativos-ti-2026"
 
     DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/gestao_ativos"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
