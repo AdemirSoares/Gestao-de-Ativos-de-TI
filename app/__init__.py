3566b2170ac514d1e780d039480331c39991535b
ademirsoares <ademir@licitaronline.com>
2026-07-01 11:28:23 -0300

    chore: configura Flask-Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
+from flask_migrate import Migrate
+
 from app.config import Config
 from app.database import db
 from app.routes import init_app
 from app import models
+
+migrate = Migrate()
 
 
 def create_app():
@@ -13,6 +17,8 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+
+    migrate.init_app(app, db)
 
     init_app(app)
 
